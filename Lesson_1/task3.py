f0ca3453a6ecada224518bcdaad6343f878e29bf
Yevhenii <jewgendrakon@gmail.com>
2022-10-18 15:28:42 +0300

    Added task 3 file

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,7 +1,7 @@
 import datetime
 
 NumberOfSeconds = int(input("Enter Number seconds - "))
-# Using the library function, we convert seconds to date
+# Using the library function, we convert seconds to date and time
 convertNumber = datetime.datetime.fromtimestamp(NumberOfSeconds)
 #the strftime() function represents the data in the specified format
 print("day and time - ", convertNumber.strftime('%d:%H:%M:%S'))
