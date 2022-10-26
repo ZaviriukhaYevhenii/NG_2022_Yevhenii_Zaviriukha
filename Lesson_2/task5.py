64259e13612bb86ba103125148ca0ee86502cd9a
Yevhenii <jewgendrakon@gmail.com>
2022-10-26 16:41:15 +0300

    Added file task 5

---
+++ Lesson_2/task5.py
@@ -0,0 +1,15 @@
+userInput = input('Enter numbers: ')
+
+index = 0
+Sum = 0
+
+userInput = userInput.split(", ")
+userInputIntegers = [int(numbers) for numbers in userInput]
+
+print("Max element - " + str(max(userInputIntegers)))
+print("Min element - " + str(min(userInputIntegers)))
+
+while index < len(userInputIntegers):
+    Sum += int(userInputIntegers[index])
+    index = index + 1
+print("The sum of all other elements - ", str(Sum - (max(userInputIntegers) +(min(userInputIntegers)))))
