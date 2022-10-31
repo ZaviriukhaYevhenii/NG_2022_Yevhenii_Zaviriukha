019e0aeb682ce39e7a9325d28c2eaaf940007879
Yevhenii <jewgendrakon@gmail.com>
2022-10-31 21:09:42 +0200

    Changed list to dictionary

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,35 +1,33 @@
 string = input("Enter sting: ")
 index = 0
 letter = ""
-sortList = []
-letterCount = []
+letterList = []
+repetitionsNumbers = []
+alphabet = {"a":"0","b":"0","c":"0","d":"0","e":"0","f":"0","g":"0","h":"0","i":"0","j":"0",
+            "k":"0","l":"0","m":"0","n":"0","o":"0","p":"0","q":"0","r":"0","s":"0","t":"0",
+            "u":"0","v":"0","w":"0","x":"0","y":"0","z":"0","A":"0","B":"0","C":"0","D":"0",
+            "E":"0","F":"0","G":"0","H":"0","I":"0","J":"0","K":"0","L":"0","M":"0","N":"0",
+            "O":"0","P":"0","Q":"0","R":"0","S":"0","T":"0","U":"0","V":"0","W":"0","X":"0",
+            "Y":"0","Z":"0"}
 
-alphabet = ["A","a","B","b","C","c","D","d","E","e","F","f",
-            "G","g","H","h","I","i","J","j","K","k","L","l",
-            "M","m","N","n","O","o","P","p","Q","q","R","r",
-            "S","s","T","t","U","u","V","v","W","w","X","x",
-            "Y","y","Z","z"]
+lengthDictionary = int(len(alphabet))
+letterList = list(alphabet.keys())
 
-while index < len(alphabet):
-    letter = string.count(alphabet[index])
-    letterCount.insert(index, letter)
-    index += 1
-
-index = 0
-print("\nI deduce the number by the letters themselves\n")
-
-while index < len(alphabet):
-    print(str(alphabet[index]) + " - " + str(letterCount[index]) + ", ", end="")
-    index += 1
-
-index = 0
-print("\n\n=======================================================\n")
-
-while index < len(alphabet):
-    if(letterCount[index] > 0):
-        print(str(alphabet[index]) + " - " + str(letterCount[index])+ ", ", end="")
-        sortList.append(letterCount[index])
+while index < lengthDictionary:
+    letter = string.count(letterList[index])
+    repetitionsNumbers.insert(index, letter)
     index += 1
     
-sortList.sort(reverse = True)
-print("\nSort - ", sortList)
+index = 0
+
+while index < lengthDictionary:
+    alphabet[letterList[index]] = repetitionsNumbers[index]
+    index += 1
+
+print("\nOutput of each letter\n", alphabet)
+
+sortDictionary = dict(sorted(alphabet.items(), key=lambda item: item[1], reverse=True))
+
+print("\nSort dictionary ")
+print(sortDictionary)
+
