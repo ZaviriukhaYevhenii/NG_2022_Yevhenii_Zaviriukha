130b4118c4f5efdbada2a6bad9538bb7e7782725
Yevhenii <jewgendrakon@gmail.com>
2022-11-10 12:58:09 +0200

    The code has been simplified

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,33 +1,18 @@
-string = input("Enter sting: ")
-index = 0
-letter = ""
-letterList = []
-repetitionsNumbers = []
-alphabet = {"a":"0","b":"0","c":"0","d":"0","e":"0","f":"0","g":"0","h":"0","i":"0","j":"0",
-            "k":"0","l":"0","m":"0","n":"0","o":"0","p":"0","q":"0","r":"0","s":"0","t":"0",
-            "u":"0","v":"0","w":"0","x":"0","y":"0","z":"0","A":"0","B":"0","C":"0","D":"0",
-            "E":"0","F":"0","G":"0","H":"0","I":"0","J":"0","K":"0","L":"0","M":"0","N":"0",
-            "O":"0","P":"0","Q":"0","R":"0","S":"0","T":"0","U":"0","V":"0","W":"0","X":"0",
-            "Y":"0","Z":"0"}
+userInput = input("Enter string - ")
+dictoonaryLetter = {}
+existingLetters = set(userInput)
 
-lengthDictionary = int(len(alphabet))
-letterList = list(alphabet.keys())
+for element in existingLetters:
+    dictoonaryLetter[element] = userInput.count(element)
+print("Number of repetitions of each letter - ", dictoonaryLetter)
 
-while index < lengthDictionary:
-    letter = string.count(letterList[index])
-    repetitionsNumbers.insert(index, letter)
-    index += 1
+listKeys = list(dictoonaryLetter.keys())
+listKeys.sort()
+print("sorting quantity by letter - ", end="")
+
+for values in listKeys:
+    print(values, '-', dictoonaryLetter[values] , " ", end="")
     
-index = 0
 
-while index < lengthDictionary:
-    alphabet[letterList[index]] = repetitionsNumbers[index]
-    index += 1
 
-print("\nOutput of each letter\n", alphabet)
-
-sortDictionary = dict(sorted(alphabet.items(), key=lambda item: item[1], reverse=True))
-
-print("\nSort dictionary ")
-print(sortDictionary)
 
